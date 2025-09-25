d7d037e26a5fb8e342dbd8d767aaec326cbff3db
Abhay <sketchologistabhay@gmail.com>
2025-09-25 06:56:13 +0530

    another try

--- src/server.js
+++ src/server.js
@@ -1,9 +1,3 @@
 import app from './app.js';
 
-
-const PORT = process.env.PORT || 3001;
-
-
-app.listen(PORT, () => {
-console.log(`Server is running on https://localhost:${PORT}`);
-});
+export default app;
